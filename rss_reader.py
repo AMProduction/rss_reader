2dcc53707347dcf5f8fa3873de591e08642494a2
Andrii Malchyk <andrii_malchyk@epam.com>
2022-08-16 11:40:35 +0300

    Main functionality added: parsing CLI arguments, getting RSS feed, showing topics as a plain text and as a JSON, possibility to limit output
    Signed-off-by: Andrii Malchyk <andrii_malchyk@epam.com>

--- rss_reader.py
+++ rss_reader.py
@@ -5,7 +5,9 @@
 
 import argparse
 
-# parsing CLI arguments
+from src.rss_reader_impl import RSSReader
+
+# adding CLI arguments
 parser = argparse.ArgumentParser(prog='RSS reader', description='Pure Python command-line RSS reader.',
                                  epilog='Enjoy the program!')
 parser.add_argument('url', type=str, help='RSS feed URL')
@@ -24,8 +26,26 @@ parser.add_argument('--limit',
                     action='store',
                     type=int,
                     help='Limit news topics if this parameter provided')
-
+# parsing CLI arguments
 args = parser.parse_args()
 
+
+# check and initial defining optional arguments
+def check_is_JSON_needed(arguments) -> bool:
+    return arguments.json
+
+
+def check_is_verbose(arguments) -> bool:
+    return arguments.verbose
+
+
+def set_limit(arguments) -> int:
+    if arguments.limit is not None and arguments.limit >= 0:
+        return arguments.limit
+    else:
+        return 0
+
+
 if __name__ == '__main__':
-    print(vars(args))
+    rss_reader = RSSReader(args.url, check_is_JSON_needed(args), check_is_verbose(args), set_limit(args))
+    rss_reader.show_rss()
--- src/rss_reader_impl.py
+++ src/rss_reader_impl.py
@@ -2,6 +2,99 @@
 #  mail: snooki17@gmail.com
 #  Licensed under the MIT License
 #  Copyright (c) 2022.
+import json
+import time
+
+import feedparser
+
 
 class RSSReader:
-    pass
+    _rss_feed_url = ""
+    _JSON_mode = False
+    _verbose_mode = False
+    _limit = 0
+
+    def __init__(self, url, is_JSON_needed=False, is_verbose=False, limit=0) -> None:
+        self._rss_feed_url = url
+        self._JSON_mode = is_JSON_needed
+        self._verbose_mode = is_verbose
+        self._limit = limit
+
+    def show_rss(self) -> None:
+        rss_feed = self._get_feed(self._rss_feed_url)
+        data = self._get_posts_details(rss_feed)
+        if self._JSON_mode:
+            self._show_rss_as_json(data)
+        else:
+            self._show_rss_as_plain_text(data)
+
+    def _get_feed(self, url):
+        return feedparser.parse(url)
+
+    def _get_posts_details(self, rss_feed) -> dict:
+        posts_details = {"Blog title": self._get_feed_name(rss_feed), "Blog link": self._get_feed_link(rss_feed),
+                         "posts": self._get_posts_list(rss_feed)}
+        return posts_details
+
+    def _get_posts_list(self, rss_feed) -> list:
+        post_list = []
+        posts = self._get_feed(self._rss_feed_url)
+        for post in posts.entries:
+            if post.title in [x['title'] for x in post_list]:
+                pass
+            else:
+                temp_post = self._get_post(post)
+                post_list.append(temp_post)
+
+        return post_list
+
+    def _get_post(self, entry) -> dict:
+        post = dict()
+        try:
+            post['title'] = entry.title
+            post['date'] = time.strftime('%Y-%m-%d', entry.published_parsed)
+            post['link'] = entry.link
+            post['links'] = [link.href for link in entry.links]
+        except:
+            pass
+
+        return post
+
+    def _get_feed_name(self, rss_feed) -> str:
+        return rss_feed.feed.title
+
+    def _get_feed_link(self, rss_feed) -> str:
+        return rss_feed.feed.link
+
+    def _show_rss_as_plain_text(self, data) -> None:
+        if self._is_print_all():
+            print(data)
+        else:
+            self._limited_print(data)
+
+    def _show_rss_as_json(self, data) -> None:
+        if self._is_print_all():
+            print(json.dumps(data, indent=2))
+        else:
+            self._limited_print(data)
+
+    def _is_print_all(self) -> bool:
+        rss_feed = self._get_feed(self._rss_feed_url)
+        feed_length = self._get_feed_length(rss_feed)
+        if self._limit == 0 or self._limit > feed_length:
+            return True
+        else:
+            return False
+
+    def _get_feed_length(self, rss_feed) -> int:
+        return len(rss_feed.entries)
+
+    def _limited_print(self, data) -> None:
+        limit = 0
+        for post in data['posts']:
+            print("********************************************************************")
+            for k, v in post.items():
+                print(k, v)
+            limit += 1
+            if limit == self._limit:
+                break
